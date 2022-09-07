fcccbc0d63c31bf6a99fd83136f7499640f006cb
rsaideekshith <87260104+rsaideekshith@users.noreply.github.com>
2022-09-07 14:55:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const aws = require('aws-sdk');
+aws.config.update({region: 'ap-south-1'});
+exports.handler = async function(event, context) {
+  const params = {
+    Message: 'Hello World!'
+   };
+  try {
+   return { statusCode: 200, body: 'Message sent' };
+  } catch(err) {
+    return { statusCode: 500, body: JSON.stringify(err) };
+  }
+};
