b2c2ea6cee41836fc610656f614fa3a4a1071b92
rsaideekshith <87260104+rsaideekshith@users.noreply.github.com>
2022-09-12 12:42:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const aws = require('aws-sdk');
 aws.config.update({region: 'ap-south-1'});
 exports.handler = async function(event, context) {
   const params = {
-    Message: 'Hello World!'
+    Message: 'Hello 1 World!'
    };
   try {
    return { statusCode: 200, body: 'Message sent' };
